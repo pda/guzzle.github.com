0bf8e621827aed9fc2abe3c487cdfb87db17101b
Michael Dowling <mtdowling@gmail.com>
2012-01-14 16:19:05 -0600

    Adding GA

--- _build_docs.sh
+++ _build_docs.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-branch=$1
-if [ "$branch" = "" ]; then
-    branch="master"
-fi
-
 if [ ! -d _docs ]; then
-    git clone git@github.com:guzzle/guzzle-docs.git _docs && cd _docs && git checkout $branch || exit 1
+    git clone git@github.com:guzzle/guzzle-docs.git _docs && cd _docs
 else
-    cd _docs && git pull $branch || exit 2
+    cd _docs && git reset --hard && git pull origin master
 fi
 
 sed -i '' 's,build/$(LANG),../,g' Makefile
 sed -i '' 's,$(SPHINXBUILD) -b html $(ALLSPHINXOPTS) $(BUILDDIR)/html,$(SPHINXBUILD) -b html $(ALLSPHINXOPTS) $(BUILDDIR),g' Makefile
-make html
+make html GA=1
